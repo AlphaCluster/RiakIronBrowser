2b1dc5f81a7bd099bb9b01bc101de37d3fff9149
AlphaCluster <AlphaCluster@linux.com>
2014-02-02 13:36:49 -0600

    Intial Checkin. Basic stuff nothing too fancy.

---
+++ RiakIronBrowser/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace RiakBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RiakBase riak = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            riak = new RiakBase();
+
+            LoadBucketList();
+        }
+
+        private void BucketLoad_Click(object sender, RoutedEventArgs e)
+        {
+            if (BucketListBox.SelectedIndex >= 0)
+            {
+                LoadKeyList();
+            }
+        }
+
+        private void DeleteBucketButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (BucketListBox.SelectedIndex >= 0)
+            {
+                IList<string> errors = riak.DeleteBucket(GetBucket());
+                StringBuilder builder = new StringBuilder("Following errors occured:");
+                foreach (string error in errors)
+                    builder.AppendLine(error);
+                MessageBox.Show(builder.ToString());
+                LoadBucketList();
+            }
+        }
+
+        private void LoadBucketList()
+        {
+            IList<string> list = riak.GetAllBuckets();
+
+            BucketListBox.Items.Clear();
+            foreach (string bucket in list)
+                BucketListBox.Items.Add(bucket);
+        }
+
+        private void LoadKeyList()
+        {
+            IList<string> keyList = riak.GetAllKeys(GetBucket());
+
+            CountLabel.Content = "Count: " + keyList.Count();
+
+            KeyListBox.Items.Clear();
+            foreach (string key in keyList)
+                KeyListBox.Items.Add(key);
+        }
+
+        private string GetBucket()
+        {
+            return (string)BucketListBox.SelectedValue;
+        }
+
+        private string GetKey()
+        {
+            return (string)KeyListBox.SelectedValue;
+        }
+
+        private void DeleteKeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((KeyListBox.SelectedIndex >= 0) &&
+                (BucketListBox.SelectedIndex >= 0))
+            {
+                riak.DeleteKey(GetBucket(), GetKey());
+                LoadKeyList();
+            }
+        }
+    }
+}
---
+++ RiakIronBrowser/RiakBase.cs
@@ -0,0 +1,58 @@
+﻿using CorrugatedIron;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiakBrowser
+{
+    public class RiakBase
+    {
+        IRiakClient client = null;
+
+        public RiakBase()
+        {
+            var cluster = RiakCluster.FromConfig("riakConfig");
+            client = cluster.CreateClient();
+        }
+
+        public IList<string> GetAllBuckets()
+        {
+            var result = client.ListBuckets();
+            return result.Value.ToList();
+        }
+
+
+        public IList<string> GetAllKeys(string bucket)
+        {
+            var result = client.ListKeys(bucket);
+            return result.Value.ToList();
+        }
+
+        /// <summary>
+        /// Deletes a complete bucket. Actually deletes all the keys in the bucket effectively deleting said bucket.
+        /// </summary>
+        /// <param name="bucket">Name of bucket to delete.</param>
+        /// <returns>Errors returned</returns>
+        public IList<string> DeleteBucket(string bucket)
+        {
+            var results = client.DeleteBucket(bucket);
+
+            IList<string> errorList = new List<string>();
+            foreach (var result in results)
+                if (!result.IsSuccess)
+                    errorList.Add(result.ErrorMessage);
+
+            return errorList;
+        }
+
+        public void DeleteKey(string bucket, string key)
+        {
+            var result = client.Delete(bucket, key);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.ErrorMessage);
+        }
+    }
+}
